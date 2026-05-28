363fe5dd412e9c5664669c1b3f3a4275d3e19077
Stick <stick.mick@gmail.com>
2026-05-29 09:30:57 +1000

    Update LSP config

--- lua/custom/plugins/lsp.lua
+++ lua/custom/plugins/lsp.lua
@@ -4,59 +4,145 @@ return {
   {
     'neovim/nvim-lspconfig',
     dependencies = {
-      'williamboman/mason.nvim',
-      'williamboman/mason-lspconfig.nvim',
+      {
+        'mason-org/mason.nvim',
+        ---@module 'mason.settings'
+        ---@type MasonSettings
+        ---@diagnostic disable-next-line: missing-fields
+        opts = {},
+      },
+      'mason-org/mason-lspconfig.nvim',
+      'WhoIsSethDaniel/mason-tool-installer.nvim',
+      { 'j-hui/fidget.nvim', opts = {} },
+      {
+        'seblyng/roslyn.nvim',
+        ft = 'cs',
+        opts = {},
+        config = function(_, opts)
+          require('roslyn').setup(opts)
+        end,
+      },
     },
     config = function()
-      require('mason').setup()
-      require('mason-lspconfig').setup({
-        ensure_installed =
-          {
-            'lua_ls',
-            'angularls',
-          },
-      })
-      local lspconfig = require('lspconfig')
-      lspconfig.lua_ls.setup({
-          on_init = function(client)
-            client.server_capabilities.documentFormattingProvider = false -- Disable formatting (formatting is done by stylua)
+      -- LspAttach keymaps and autocmds
+      vim.api.nvim_create_autocmd('LspAttach', {
+        group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
+        callback = function(event)
+          local map = function(keys, func, desc, mode)
+            mode = mode or 'n'
+            vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
+          end
 
-            if client.workspace_folders then
-              local path = client.workspace_folders[1].name
-              if path ~= vim.fn.stdpath 'config' and (vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc')) then return end
-            end
+          map('grn', vim.lsp.buf.rename, '[R]e[n]ame')
+          map('gra', vim.lsp.buf.code_action, '[G]oto Code [A]ction', { 'n', 'x' })
+          map('grD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
 
-            client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
-              runtime = {
-                version = 'LuaJIT',
-                path = { 'lua/?.lua', 'lua/?/init.lua' },
-              },
-              workspace = {
-                checkThirdParty = false,
-                -- NOTE: this is a lot slower and will cause issues when working on your own configuration.
-                --  See https://github.com/neovim/nvim-lspconfig/issues/3189
-                library = vim.tbl_extend('force', vim.api.nvim_get_runtime_file('', true), {
-                  '${3rd}/luv/library',
-                  '${3rd}/busted/library',
-                }),
-              },
+          local client = vim.lsp.get_client_by_id(event.data.client_id)
+          if client and client:supports_method('textDocument/documentHighlight', event.buf) then
+            local highlight_augroup = vim.api.nvim_create_augroup('kickstart-lsp-highlight', { clear = false })
+            vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
+              buffer = event.buf,
+              group = highlight_augroup,
+              callback = vim.lsp.buf.document_highlight,
             })
-          end,
-          ---@type lspconfig.settings.lua_ls
-          settings = {
-            Lua = {
-              format = { enable = false }, -- Disable formatting (formatting is done by stylua)
-            },
-          },
-      })
-      lspconfig.angularls.setup({
-        filetypes = { 'typescript', 'html', 'typescriptreact', 'typescript.tsx' },
-        on_attach = function(client, bufnr)
-          -- Disable formatting capabilities for Angular Language Server
-          client.server_capabilities.documentFormattingProvider = false
-          client.server_capabilities.documentRangeFormattingProvider = false
+
+            vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
+              buffer = event.buf,
+              group = highlight_augroup,
+              callback = vim.lsp.buf.clear_references,
+            })
+
+            vim.api.nvim_create_autocmd('LspDetach', {
+              group = vim.api.nvim_create_augroup('kickstart-lsp-detach', { clear = true }),
+              callback = function(event2)
+                vim.lsp.buf.clear_references()
+                vim.api.nvim_clear_autocmds({ group = 'kickstart-lsp-highlight', buffer = event2.buf })
+              end,
+            })
+          end
+
+          if client and client:supports_method('textDocument/inlayHint', event.buf) then
+            map(
+              '<leader>th',
+              function()
+                vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = event.buf }))
+              end,
+              '[T]oggle Inlay [H]ints'
+            )
+          end
         end,
       })
+
+      -- Mason setup
+      require('mason-tool-installer').setup({
+        ensure_installed = { 'lua_ls', 'ts_ls', 'angularls', 'stylua' },
+      })
+
+      -- Lua Language Server
+      vim.lsp.config('lua_ls', {
+        on_init = function(client)
+          client.server_capabilities.documentFormattingProvider = false
+
+          if client.workspace_folders then
+            local path = client.workspace_folders[1].name
+            if
+              path ~= vim.fn.stdpath('config')
+              and (vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc'))
+            then
+              return
+            end
+          end
+
+          client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+            runtime = {
+              version = 'LuaJIT',
+              path = { 'lua/?.lua', 'lua/?/init.lua' },
+            },
+            workspace = {
+              checkThirdParty = false,
+              library = vim.tbl_extend('force', vim.api.nvim_get_runtime_file('', true), {
+                '${3rd}/luv/library',
+                '${3rd}/busted/library',
+              }),
+            },
+          })
+        end,
+        settings = {
+          Lua = {
+            format = { enable = false },
+          },
+        },
+      })
+
+      -- TypeScript Language Server
+      vim.lsp.config('ts_ls', {
+        cmd = { 'typescript-language-server', '--stdio' },
+        filetypes = { 'javascript', 'javascriptreact', 'typescript', 'typescriptreact' },
+        root_dir = vim.fs.root(0, { 'package.json', 'tsconfig.json', 'jsconfig.json' }),
+      })
+
+      -- Angular Language Server
+      vim.lsp.config('angularls', {
+        cmd = function()
+          local root_dir = vim.fs.root(0, { 'angular.json', 'project.json' }) or vim.fn.getcwd()
+          return {
+            'ngserver',
+            '--stdio',
+            '--tsProbeLocations', root_dir,
+            '--ngProbeLocations', root_dir,
+          }
+        end,
+        filetypes = { 'typescript', 'html', 'typescriptreact', 'typescript.tsx' },
+        root_dir = vim.fs.root(0, { 'angular.json', 'project.json' }),
+      })
+
+      -- Stylua formatter config
+      vim.lsp.config('stylua', {})
+
+      -- Enable LSP servers
+      vim.lsp.enable('ts_ls')
+      vim.lsp.enable('angularls')
+      vim.lsp.enable('stylua')
     end,
   },
 }
